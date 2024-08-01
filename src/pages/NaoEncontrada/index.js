f8556c19f45f235b018095750576837c036d9acc
Ariel Spencer <arielspencer.tech@gmail.com>
2024-08-01 12:44:09 -0300

    Implementar banner da página 404 não encontrada

--- src/pages/NaoEncontrada/index.js
+++ src/pages/NaoEncontrada/index.js
@@ -1,20 +1,24 @@
 import { Link } from "react-router-dom";
 import styles from "./NaoEncontrada.module.css";
+import Banner from "components/Banner";
 
 function NaoEncontrada() {
   return (
-    <section className={styles.container}>
-      <div>
-        <h2>Ops!</h2>
-        <p>O conteúdo que você procura não foi encontrado.</p>
-      </div>
-      <Link
-        to="/"
-        className={styles.link}
-      >
-        Retornar a página inicial
-      </Link>
-    </section>
+    <>
+      <Banner imagem="nao-encontrada" />
+      <section className={styles.container}>
+        <div>
+          <h2>Ops!</h2>
+          <p>O conteúdo que você procura não foi encontrado.</p>
+        </div>
+        <Link
+          to="/"
+          className={styles.link}
+        >
+          Retornar a página inicial
+        </Link>
+      </section>
+    </>
   )
 }
 
