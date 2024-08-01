4a4947249390be96c1de3fcdcd716a005fa54582
Ariel Spencer <arielspencer.tech@gmail.com>
2024-07-31 23:38:28 -0300

    Implementar design dos cards

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,12 +1,18 @@
 import Banner from "components/Banner";
 import Cabecalho from "components/Cabecalho";
+import Card from "components/Card";
 import Rodape from "components/Rodape";
+import Titulo from "components/Titulo";
 
 function Inicio() {
   return (
     <>
       <Cabecalho />
       <Banner imagem="home" />
+      <Titulo>
+        <h1>Guarde seus vídeos e filmes com facilidade aqui!</h1>
+      </Titulo>
+      <Card id="1" titulo="Gato Bonifácio" capa="https://i.pinimg.com/564x/f2/66/3d/f2663dbf252d2a5cfa8995cec762f3cf.jpg" />
       <Rodape />
     </>
   )
