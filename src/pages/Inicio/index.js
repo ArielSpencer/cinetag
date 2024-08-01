8c0d1a09faf01ed466c67ff7155a0306b70b0552
Ariel Spencer <arielspencer.tech@gmail.com>
2024-07-31 23:46:29 -0300

    Importar lista de cards com base no json

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -3,6 +3,8 @@ import Cabecalho from "components/Cabecalho";
 import Card from "components/Card";
 import Rodape from "components/Rodape";
 import Titulo from "components/Titulo";
+import videos from "json/db.json";
+import styles from "./Inicio.module.css";
 
 function Inicio() {
   return (
@@ -12,7 +14,11 @@ function Inicio() {
       <Titulo>
         <h1>Guarde seus vídeos e filmes com facilidade aqui!</h1>
       </Titulo>
-      <Card id="1" titulo="Gato Bonifácio" capa="https://i.pinimg.com/564x/f2/66/3d/f2663dbf252d2a5cfa8995cec762f3cf.jpg" />
+      <section className={styles.container}>
+        {videos.map((video) => {
+          return <Card {...video} key={video.id} />
+        })}
+      </section>
       <Rodape />
     </>
   )
