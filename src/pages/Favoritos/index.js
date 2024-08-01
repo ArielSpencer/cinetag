af9a239732ab9eabc45c7091d5c0803945775c3c
Ariel Spencer <arielspencer.tech@gmail.com>
2024-08-01 10:37:38 -0300

    Mostrar lista de favoritos na página de favoritos

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -2,8 +2,10 @@ import Banner from "components/Banner";
 import styles from "./Favoritos.module.css";
 import Titulo from "components/Titulo";
 import Card from "components/Card";
+import { useFavoritoContext } from "contextos/Favoritos";
 
 function Favoritos() {
+  const { favorito } = useFavoritoContext();
   return (
     <>
       <Banner imagem="favoritos" />
@@ -11,7 +13,9 @@ function Favoritos() {
         <h1>Meus Favoritos</h1>
       </Titulo>
       <section className={styles.container}>
-        <Card id="1" titulo="Gato Bonifácio" capa="https://i.pinimg.com/564x/f2/66/3d/f2663dbf252d2a5cfa8995cec762f3cf.jpg" />
+        {favorito.map((fav) => {
+          return <Card {...fav} key={fav.id} />
+        })}
       </section>
     </>
   )
